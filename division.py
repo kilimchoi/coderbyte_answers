25a5deb46ba26a6872c85e3fbece01c2a61d239a
kilimchoi <stevn1202@gmail.com>
2014-03-27 22:34:08 -0700

    adding gcd problem

---
+++ division.py
@@ -0,0 +1,15 @@
+from math import sqrt
+def Division(num1, num2):
+	factors = []
+	greatest_divisor_so_far = 1
+	for n in range(2, num1+1):
+		if num1 % n == 0:
+			factors.append(n)
+	for factor in factors:
+		if num2 % factor == 0:
+			if factor > greatest_divisor_so_far:
+			   greatest_divisor_so_far = factor
+	return greatest_divisor_so_far
+
+print Division(12, 16)
+print Division(3, 9)
