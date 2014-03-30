269762a1254894dd1315582b85a11b2d1d9c2a5d
kilimchoi <stevn1202@gmail.com>
2014-03-29 22:21:08 -0700

    optimized version of lettercount.py

--- lettercount.py
+++ lettercount.py
@@ -1,26 +1,17 @@
 def LetterCount(str): 
-  repeating_count = 0
-  to_return = ""
   str_arr = str.split()
-  repeating_count = 0
+  max_count, res = 1, -1
   for word in str_arr:
-    repeating_dict = {}
-    is_changed = False
-    for i in range(0, len(word)):
-      letter = word[i]
-      if letter not in repeating_dict:
-        repeating_dict[letter] = 1
-      else:
-        repeating_dict[letter] += 1
-    for count in repeating_dict.values():
-      if count > repeating_count:
-        is_changed = True
-        repeating_count = count
-
-    if is_changed and repeating_count != 1:
-      to_return = word
-  if not is_changed and repeating_count == 1:
-    return -1
+    c = 1
+    for ch in word:
+      n = word.count(ch)
+      if n > c:
+        c = n
+    if c > max_count:
+      max_count = c
+      res = word
+  return res
+    
   return to_return
 print LetterCount("hello apple pie")
 print LetterCount("No Words")
